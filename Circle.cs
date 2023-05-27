97b36d3a754ce35436a8eb5f0ee501ea772b841f
OlenA-Korenyuk <0631952502c@gmail.com>
2023-05-27 20:53:52 +0300

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Circle
+    {
+        private Point center;
+        private double radius;
+
+        public Circle(Point center, double radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        public void SetRadius(double radius)
+        {
+            if (radius > 0)
+            {
+                this.radius = radius;
+            }
+            else
+            {
+                Console.WriteLine("Радіус не може бути від'ємним");
+            }
+
+        }
+
+        public bool ContainsPoint(Point point)
+        {
+            double distance = Math.Sqrt(Math.Pow(point.X - center.X, 2) + Math.Pow(point.Y - center.Y, 2));
+            return distance <= radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Point center = new Point(0, 0);
+            double radius = 5;
+            Circle circle = new Circle(center, radius);
+
+            Console.WriteLine("Центр кола: ({0}, {1})", center.X, center.Y);
+            Console.WriteLine("Радіус кола: {0}", radius);
+
+            Point point1 = new Point(3, 4);
+            Console.WriteLine("Точка ({0}, {1})  {2} колу", point1.X, point1.Y, circle.ContainsPoint(point1) ? "належить" : "не належить");
+
+            Point point2 = new Point(6, 7);
+            Console.WriteLine("Точка ({0}, {1})  {2} колу ", point2.X, point2.Y, circle.ContainsPoint(point2) ? "належить" : "не належить");
+
+            Console.WriteLine("Введіть значення х точки");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення у для точки");
+            int m = int.Parse(Console.ReadLine());
+            Point point3 = new Point(n, m);
+            Console.WriteLine("Точка ({0}, {1})  {2} колу ", point3.X, point3.Y, circle.ContainsPoint(point3) ? "належить" : "не належить");
+
+            Console.Write("Введіть нове значення радіусу кола: ");
+            int NewRadius = int.Parse(Console.ReadLine());
+            circle.SetRadius(NewRadius);
+            Console.WriteLine("Радіус кола змінено на {0}", circle.Radius);
+            Console.WriteLine("Центр кола: ({0}, {1})", center.X, center.Y);
+            Console.WriteLine("Радіус кола: {0}", circle.Radius);
+
+            Console.WriteLine("Введіть значення х точки");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення у для точки");
+            int l = int.Parse(Console.ReadLine());
+            Point point4 = new Point(k, l);
+            Console.WriteLine("Точка ({0}, {1})  {2} колу ", point4.X, point4.Y, circle.ContainsPoint(point4) ? "належить" : "не належить");
+
+            Console.ReadLine();
+        }
+        catch(System.FormatException)
+        {
+            Console.WriteLine("Введіть число!");
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Введіть число!");
+        }
+    }
+}
+
+
+
+
